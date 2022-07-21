437319dc3d56e8a778555318ffab0b29c70105a2
Federico Juretich <fedejuret@gmail.com>
2022-07-21 19:59:06 -0300

    add product controller

---
+++ src/controllers/product.js
@@ -0,0 +1,12 @@
+
+
+const renderUploadProductView = (req, res) => {
+
+    return res.render('products');
+
+}
+
+
+module.exports = {
+    renderUploadProductView
+};
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,12 +1,9 @@
 const express = require('express');
+const { renderUploadProductView } = require('../controllers/product');
 const router = express.Router();
 
 
-router.get('/cargar-producto', (req, res) => {
-
-    res.render('products');
-
-});
+router.get('/cargar-producto', renderUploadProductView);
 
 
 module.exports = router;
